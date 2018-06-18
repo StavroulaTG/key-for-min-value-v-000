0c606900043ec2db0cbded78c93d6f7e9e65200f
Stavroula Guerrero <4stacyg@optonline.net>
2018-06-18 02:03:39 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  cars = {:Porsche => 25, :Lamborghini => 50, :Ferrari => 100}
   cars.collect do |cars, num|
+    if num < smallest
+      name_hash
+    end
   end
-  num <=
 end
